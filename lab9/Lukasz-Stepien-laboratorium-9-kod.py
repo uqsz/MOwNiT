808d0f40fc4f8250af53cbf9f1c10b3c5e18895a
Łukasz Stępień <uqsz123@gmail.com>
2023-05-16 12:06:16 +0200

    lab

--- lab9/Lukasz-Stepien-laboratorium-9-kod.py
+++ lab9/Lukasz-Stepien-laboratorium-9-kod.py
@@ -48,6 +48,38 @@ plt.title('Niejawna metoda Eulera dla kroku h=0.5')
 plt.grid(True)
 plt.savefig(f"pictures/Niejawna metoda Eulera dla kroku h=0,5", dpi=350)
 plt.show()
+
+def f(x,y,t):
+    return (x**2+y**2)**(-1.5)
+
+def euler_mul(a,b,n):
+    Tx=[1]
+    Ty=[0]
+    Tvx=[0]
+    Tvy=[1]
+    Tt=[0]
+    h=(b-a)/n
+    t=0
+    i=0
+    while t<b:
+        Tvx.append(Tvx[i]-Tx[i]*f(Tx[i],Ty[i],t)*h)
+        Tx.append(Tx[i]+Tvx[i]*h)
+        Tvy.append(Tvy[i]-Ty[i]*f(Tx[i],Ty[i],t)*h)
+        Ty.append(Ty[i]+Tvy[i]*h)
+        Tt.append(t)
+        i+=1
+        t+=h
+    return Tvx,Tx,Tvy,Ty,Tt
+        
+Tvx,Tx,Tvy,Ty,Tt=euler_mul(0,100,20000)
+
+plt.plot(Ty,Tx, label='y1')
+plt.xlabel('Czas')
+plt.ylabel('y1')
+plt.title('Wykres y1')
+plt.legend()
+plt.grid(True)
+plt.show()
     
 
 
